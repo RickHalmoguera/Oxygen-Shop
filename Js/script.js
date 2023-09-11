a28431914e4686937878aee31898c19919ef7011
Ricardo Halmoguera <halmoguerar@gmail.com>
2023-09-11 16:17:19 +0200

    form validation styled and fix

--- Js/script.js
+++ Js/script.js
@@ -4,6 +4,8 @@ const scrollBar = document.getElementById("scrollBar")
 const backToTop = document.getElementById("backToTop")
 const contactForm = document.getElementById("contactForm")
 const nameInput = document.getElementById("nameInput")
+const nameError = document.getElementById("nameError")
+const emailError = document.getElementById("emailError")
 const emailInput = document.getElementById("emailInput")
 const checkboxInput = document.getElementById("checkboxInput")
 const contactBtn = document.getElementById("contactBtn")
@@ -13,6 +15,9 @@ const menuSrc ="./assets/Menu.svg"
 
 let percent = 0
 let isOpen = false
+let isValid
+let nameIsValid
+let emailIsValid
 
 const handleMenu = () =>{
     console.log("Hola")
@@ -51,58 +56,61 @@ const formOk = () =>{
     
 }
 
-const handleSubmit = (e) => {
 
-    if (nameInput.value.length < 2 || nameInput.value.length > 100) {
-        console.log("entro")
-        nameInput.setCustomValidity("The name must have 2 or more characters")
-        nameInput.classList.add("input__error")
-        e.preventDefault()
-    } else {
-        nameInput.setCustomValidity("")
-        nameInput.classList.remove("input__error")
+const checkName = ()=>{
+    
+    if (nameInput.value.length < 2 || nameInput.value.length >100) {
+        nameError.style.visibility ="visible"
+        nameInput.classList.add("input-error")
+        nameIsValid = false
+      } else {
+        nameIsValid = true
+        nameError.style.visibility ="hidden"
+        nameInput.classList.remove("input-error")
+      }
+}
+
+const checkEmail = ()=>{
+    const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+    if(!emailRegex.test(emailInput.value)){
+        emailError.style.visibility="visible"
+        emailInput.classList.add("input-error")
+        emailIsValid = false
+    }else {
+        emailIsValid = true
+        emailError.style.visibility="hidden"
+        emailInput.classList.remove("input-error")
+      }
+}
+
+const handleSubmit = (e) =>{
+    e.preventDefault()
+
+    if(nameIsValid && emailIsValid){
+        fetch('https://jsonplaceholder.typicode.com/posts/1', {
+            method: 'PUT',
+            body: JSON.stringify({
+            id: 1,
+            name: nameInput.value,
+            email: emailInput.value,
+            userId: 1,
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        })
+        .then((response) => response.json())
+        .then((json) => console.log(json));
+    }else{
+        checkName()
+        checkEmail()
     }
-
-    const emailValidation = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (emailInput.value.length === 0 || !emailValidation.test(emailInput.value)) {
-        emailInput.setCustomValidity("The email must be valid")
-        emailInput.classList.add("input__error")
-        e.preventDefault()
-    } else {
-        emailInput.setCustomValidity("")
-        emailInput.classList.remove("input__error")
-    }
-
-    if (checkboxInput.checked === false) {
-        checkboxInput.setCustomValidity("You must accept the conditions")
-        checkboxInput.classList.add("input__error")
-        e.preventDefault()
-    } else {
-        checkboxInput.setCustomValidity("")
-        checkboxInput.classList.remove("input__error")
-    }
 }
-
-const checkName = () =>{
-    nameInput.setCustomValidity("")
-    nameInput.classList.remove("input__error")
-}
-
-const checkEmail = () =>{
-    emailInput.setCustomValidity("")
-    emailInput.classList.remove("input__error")
-}
-
-const checkCheckBox = () =>{
-    checkboxInput.setCustomValidity("")
-    checkboxInput.classList.remove("input__error")
-}
-
 
 menuBtn.addEventListener("click", handleMenu)
 window.addEventListener("scroll", handleScroll)
 backToTop.addEventListener("click",handleClick)
-contactForm.addEventListener("submit",handleSubmit)
 nameInput.addEventListener("input",checkName)
-emailInput.addEventListener("input", checkEmail)
-checkboxInput.addEventListener("change", checkCheckBox)
+emailInput.addEventListener("input",checkEmail)
+contactForm.addEventListener("submit", handleSubmit)
