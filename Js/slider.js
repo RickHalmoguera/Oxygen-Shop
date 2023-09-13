19d767a20c3562956d7f2aeeb8e027d0e8a94387
Ricardo Halmoguera <halmoguerar@gmail.com>
2023-09-13 14:15:40 +0200

    slider working and styled

--- Js/slider.js
+++ Js/slider.js
@@ -6,21 +6,28 @@ class slider {
         "./assets/image3.jpg",
         "./assets/image4.jpg",];
         this.slide = document.getElementById("sliderImage")
-        this.next = document.getElementById('nextBtn')
-        this.previous = document.getElementById('prevBtn')
+        this.next = document.getElementById("nextBtn")
+        this.previous = document.getElementById("prevBtn")
         this.index = 0;
         this.slideLength =this.slides.length
+        this.dots = document.querySelectorAll(".slider__dot");
         
-        this.next.addEventListener("click",this.nextSlide)
-        this.previous.addEventListener("click",this.prevSlide)
+        this.next.addEventListener("click",()=>this.nextSlide())
+        this.previous.addEventListener("click",()=>this.prevSlide())
     }
     
+    updateDots(dot){
+        dot.classList.remove("active")
+    }
+
     nextSlide() {
         this.index++;
         if (this.index >= this.slideLength)  {
             this.index = 0;
         }
         this.slide.src = this.slides[this.index];
+        this.dots.forEach((dot) => this.updateDots(dot))
+        this.dots[this.index].classList.add("active")
     }
 
     prevSlide(){
@@ -28,9 +35,10 @@ class slider {
         if (this.index < 0 ) {
             this.index = this.slideLength - 1
         }
-        this.src = this.slides[this.index]
+        this.slide.src = this.slides[this.index]
+        this.dots.forEach((dot) => this.updateDots(dot))
+        this.dots[this.index].classList.add("active")
     }
-
 
   };
   
