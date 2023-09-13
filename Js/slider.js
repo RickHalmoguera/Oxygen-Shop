773e9ac11ad323bffb2dea5d85aff229023d93df
Ricardo Halmoguera <halmoguerar@gmail.com>
2023-09-13 11:57:00 +0200

    slider class updated with new Methods

--- Js/slider.js
+++ Js/slider.js
@@ -1,31 +1,37 @@
 class slider {
     constructor() {
-      this.slide = [
+        this.slides = [
         "./assets/image1.jpg",
         "./assets/image2.jpg",
         "./assets/image3.jpg",
         "./assets/image4.jpg",];
-      this.next = document.getElementById('nextBtn')
-      this.previous = document.getElementById('prevBtn')
-      this.index = 0;
-      this.slideLength =this.slide.length
-      this.next.onclick = () => {
-        this.index++;
-          if (this.index >= this.slideLength)  {
-              this.index = 0;
-            }
-        document.getElementById("sliderImage").src = this.slide[this.index];
-        console.log(this.index);
-        console.log(this.slideLength)
-      }
-      this.previous.onclick = () => {
-        this.index--
-          if (this.index < 0 ) {
-              this.index = this.slideLength - 1
-            }
-        document.getElementById("sliderImage").src = this.slide[this.index]
-      }
+        this.slide = document.getElementById("sliderImage")
+        this.next = document.getElementById('nextBtn')
+        this.previous = document.getElementById('prevBtn')
+        this.index = 0;
+        this.slideLength =this.slides.length
+        
+        this.next.addEventListener("click",this.nextSlide)
+        this.previous.addEventListener("click",this.prevSlide)
     }
+    
+    nextSlide() {
+        this.index++;
+        if (this.index >= this.slideLength)  {
+            this.index = 0;
+        }
+        this.slide.src = this.slides[this.index];
+    }
+
+    prevSlide(){
+        this.index--
+        if (this.index < 0 ) {
+            this.index = this.slideLength - 1
+        }
+        this.src = this.slides[this.index]
+    }
+
+
   };
   
   new slider();
